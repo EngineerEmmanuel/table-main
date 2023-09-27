9b164a3e713b355dc9e173b54c9a242adc2e8d33
emmanuel <emmanuelchukwuemeka970@gmail.com>
2023-09-27 22:52:40 +0100

    Initial commit from Create Next App

---
+++ app/Transaction/Page.jsx
@@ -0,0 +1,26 @@
+"use client"
+const Transaction = () => {
+    return ( 
+        <section>
+            {/* general container */}
+            <div>
+                {/* txn hash */}
+                <div>
+                    <p>Txn hash</p>
+                </div>
+                {/* status */}
+                <div>
+                    <p>Status</p>
+                </div>
+                {/* block */}
+                <div>
+                    <p>Block</p>
+
+                </div>
+                {/*  */}
+            </div>
+        </section>
+     );
+}
+ 
+export default Transaction;
